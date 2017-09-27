8e3fafa23d78bb25d8d29eea31ca72d561b7acd1
Brendan Davis <BrendanMD@gmail.com>
2017-09-27 11:02:44 -0700

    working on hangman

--- hangman.py
+++ hangman.py
@@ -1,32 +1,93 @@
 from random import *
 list = ['consider', 'minute', 'accord']
-answer = list[randint(0,2)] #The answer in the format of a word
-answerList = answer.split() #The answer in the form of a list - for checking
-numLetters = len(answer) #The number of letters for the answer
+rightAnswer = list[randint(0,2)] #The answer in the form of a list
+answerList = rightAnswer.split() #The answer in the form of a word
+numLetters = len(rightAnswer) #The number of letters for the answer
+theirTrack = numLetters * "_"
 theirView = [] #The portion of the puzzle they have solved - or blank spaces if they haven't. 
 guesses = 6 #Guesses remaining
+hangedMan = {} #for the drawings of the hanged man
 
-i = 0 #Tracker
-
-while i < numLetters: #Populate theirView with an underscore for each letter of answer
+for i in rightAnswer:
     theirView.append("_")
-    i += 1
 
 theirLetters = " ".join(theirView) #Creates a word from the list of Theirview - with a space between to display underscores
 guessPhrase =  "You have " + str(guesses) + " guesses remaining."
 wordPhrase = "You know your word has the following letters. " + theirLetters
-print answer
+
 def printout():
     print guessPhrase
     print wordPhrase
 
 def checkAnswer(input):
-    for item in answerList:
-        if input = item:
-            print "Yes it's working"
+    matchInWord = 0
+    for a, item in enumerate(rightAnswer): #cycle through the answer list
+        if input == item:
+            matchInWord += 1
+            print "That's in the word!"
+            theirView[a] = input
+    if matchInWord < 1:
+        guesses += -1
+    print hangedMan[guesses]    
+            
 
+hangedMan[0] = """                                   
+                    _____
+                    O    |    
+                  / | \\  |
+                   / \\   |
+                         |
+   """
+hangedMan[1] = """                                   
+                    _____
+                    O    |    
+                  / | \\  |
+                   /     |
+                         |
+   """
 
-print "Let's play Hangman!"
+hangedMan[2] = """                                   
+                    _____
+                    O    |    
+                  / | \\  |
+                         |
+                         |
+   """
+
+hangedMan[3] = """                                   
+                    _____
+                    O    |    
+                  / |    |
+                         |
+                         |
+   """
+
+hangedMan[4] = """                                   
+                    _____
+                    O    |    
+                    |    |
+                         |
+                         |
+   """
+
+hangedMan[5] = """                                   
+                    _____
+                    O    |    
+                         |
+                         |
+                         |
+   """
+
+hangedMan[6] = """                                   
+                    _____
+                         |    
+                         |
+                         |
+                         |
+   """
+
+print hangedMan[6]
+
 printout()
 
 checkAnswer(raw_input('Which letter would you like to check?'))
